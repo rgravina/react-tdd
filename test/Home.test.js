31ac1ed5f9df289cfebff7877c277882841f9a35
Robert Gravina <robert.gravina@gmail.com>
2019-01-31 08:51:26 +0900

    Add hello world test

--- test/Home.test.js
+++ test/Home.test.js
@@ -1,10 +1,13 @@
 import React from "react"
 import assert from "assert"
+import expect from "expect"
 import {shallow} from "enzyme"
 import Home from "../src/Home"
 
 describe('Home', () => {
     it('renders', () => {
       const wrapper = shallow(<Home/>)
+
+      expect(wrapper.text()).toContain("Hello World")
     });
 });
