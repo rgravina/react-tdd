8094f9607971273afdd02a6460bd2a271436107c
Robert Gravina <robert.gravina@gmail.com>
2019-01-31 17:29:50 +0900

    Update dog images.

--- src/DogRepository.js
+++ src/DogRepository.js
@@ -1,7 +1,7 @@
 class DogRepository {
     getDogImages() {
       return [
-        "https://images.dog.ceo/breeds/mix/Polo.jpg",
+        "https://images.dog.ceo/breeds/chow/n02112137_16109.jpg",
         "https://images.dog.ceo/breeds/akita/512px-Akita_inu.jpeg"
       ]
     }
