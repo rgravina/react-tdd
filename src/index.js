4f15eba9651d20698b59579ce63c17ccb59bf053
Robert Gravina <robert.gravina@gmail.com>
2019-01-31 17:18:22 +0900

    Remove image urls from index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,8 @@ import ReactDOM from "react-dom"
 import Home from "./Home"
 import DogRepository from "./DogRepository"
 
-const images = [
-    "https://images.dog.ceo/breeds/mix/Polo.jpg",
-    "https://images.dog.ceo/breeds/akita/512px-Akita_inu.jpeg"
-]
-
 const dogRepository = new DogRepository()
 
 ReactDOM.render(<Home 
-    images={images} 
     dogRepository={dogRepository}
 />, document.getElementById("app"));
